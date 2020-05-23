7755cb769e5dc622d3366f0caab5f2a4f1a52f3b
utsugiriso <utsugiriso@gmail.com>
2020-05-24 05:35:38 +0900

    bug fix

--- restart.ps1
+++ restart.ps1
@@ -9,7 +9,7 @@ Param(
     [string]$sqlitePath,
     [string]$sqliteBackupDirPath,
     [int]$difficulty, # 0(classic), 1(expert), 2(master), 3(journey)
-    [string]$worldNamePrefix,
+    [string]$worldName,
     [string]$worldDirPath,
     [string]$worldBackupDirPath,
     [string]$vanillaServerDirPath,
@@ -25,7 +25,7 @@ Write-Output  "password:`t$($password)"
 Write-Output  "sqlitePath:`t$($sqlitePath)"
 Write-Output  "sqliteBackupDirPath:`t$($sqliteBackupDirPath)"
 Write-Output  "difficulty:`t$($difficulty)"
-Write-Output  "worldNamePrefix:`t$($worldNamePrefix)"
+Write-Output  "worldName:`t$($worldName)"
 Write-Output  "worldDirPath:`t$($worldDirPath)"
 Write-Output  "worldBackupDirPath:`t$($worldBackupDirPath)"
 Write-Output  "vanillaServerDirPath:`t$($vanillaServerDirPath)"
@@ -36,41 +36,43 @@ Write-Output  "tshockDirPath:`t$($tshockDirPath)"
 ################################
 $rootUrl = "http://localhost:$($apiPort)"
 $token = ((Invoke-WebRequest -URI "$($rootUrl)/v2/token/create" -Method GET -Body @{username = $username; password = $password }).Content | ConvertFrom-Json).token
-$message = "This server will shut down in {0} minutes."
-$broadcastUrl = "$($rootUrl)/v2/server/broadcast"
+if (![string]::IsNullOrEmpty($token)){
+    $message = "This server will shut down in {0} minutes."
+    $broadcastUrl = "$($rootUrl)/v2/server/broadcast"
+    
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 10) }
+    timeout 300
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 5) }
+    timeout 120
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 3) }
+    timeout 60
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 2) }
+    timeout 60
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 1) }
+    timeout 55
+    
+    $message = "{0}"
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 5) }
+    timeout 1
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 4) }
+    timeout 1
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 3) }
+    timeout 1
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 2) }
+    timeout 1
+    Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 1) }
+    timeout 1
+    
+    $message = if ($wipe) {
+        'See you in new world a few minutes later!'
+    }
+    else {
+        'See you in a few minutes later!'
+    }
+    Invoke-WebRequest -URI "$($rootUrl)/v2/server/off" -Method GET -Body @{token = $token; confirm = 'true'; message = $message; nosave = 'false' }
 
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 10) }
-Start-Sleep -s 300
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 5) }
-Start-Sleep -s 120
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 3) }
-Start-Sleep -s 60
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 2) }
-Start-Sleep -s 60
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 1) }
-Start-Sleep -s 55
-
-$message = "{0}"
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 5) }
-Start-Sleep -s 1
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 4) }
-Start-Sleep -s 1
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 3) }
-Start-Sleep -s 1
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 2) }
-Start-Sleep -s 1
-Invoke-WebRequest -URI $broadcastUrl -Method GET -Body @{token = $token; msg = [String]::Format($message, 1) }
-Start-Sleep -s 1
-
-$message = if ($wipe) {
-    'See you in new world a few minutes later!'
+    timeout 10 # サーバーシャットダウンを待つ
 }
-else {
-    'See you in a few minutes later!'
-}
-Invoke-WebRequest -URI "$($rootUrl)/v2/server/off" -Method GET -Body @{token = $token; confirm = 'true'; message = $message; nosave = 'false' }
-
-Start-Sleep -s 10 # サーバーシャットダウンを待つ
 
 ################################
 # バックアップ
@@ -78,15 +80,13 @@ Start-Sleep -s 10 # サーバーシャットダウンを待つ
 $dateString = Get-Date -UFormat "%Y-%m-%d %H-%M-%S"
 
 # DB
-$sqliteBackupPath = Join-Path $sqliteBackupDirPath $dateString
-New-Item $sqliteBackupPath -ItemType Directory
-if (![string]::IsNullOrEmpty($sqlitePath)) {
+if (![string]::IsNullOrEmpty($sqlitePath) -and ![string]::IsNullOrEmpty($sqliteBackupDirPath)) {
+    $sqliteBackupPath = Join-Path $sqliteBackupDirPath $dateString
+    New-Item $sqliteBackupPath -ItemType Directory
     Write-Output "copy $($sqlitePath) to $($sqliteBackupPath)"
     Copy-Item $sqlitePath $sqliteBackupPath
 }
 
-# World 0(classic), 1(expert), 2(master), 3(journey)
-$worldName = "$($worldNamePrefix)_$($difficulty)"
 $worldFileName = "$($worldName).wld"
 $worldPath = Join-Path $worldDirPath $worldFileName
 $worldBackupPath = Join-Path  $worldBackupDirPath $dateString
@@ -107,12 +107,13 @@ else {
 if ($wipe) {
     $vanillaServerPath = Join-Path $vanillaServerDirPath TerrariaServer.exe
     $serverConfigPath = Join-Path $vanillaServerDirPath "serverconfig_$($difficulty).txt"
+    Write-Output "beginning create world"
     $createWorldProcess = Start-Process -FilePath $vanillaServerPath -ArgumentList "-port 9999 -worldname `"$($worldName)`" -world `"$($worldPath)`" -autocreate 3 -config `"$($serverConfigPath)`"" -PassThru
     
     # .wldファイルができるまで最大10分待つ
-    for ($i = 0; $i -eq 20; $i++) {
-        Start-Sleep -s 30 # 30秒待つ
-        if ([System.IO.File]::Exists($worldPath)){
+    for ($i = 0; $i -lt 20; $i++) {
+        timeout 30 # 30秒待つ
+        if ([System.IO.File]::Exists($worldPath)) {
             break
         }
     }
@@ -124,4 +125,4 @@ if ($wipe) {
 # サーバー起動
 ################################
 Set-Location -Path $tshockDirPath
-Start-Process -FilePath TerrariaServer.exe -ArgumentList "-world `"$($worldPath)`""
+Start-Process -FilePath TerrariaServer.exe -ArgumentList "-world `"$($worldFileName)`""
